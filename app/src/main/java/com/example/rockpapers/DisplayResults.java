23463090c6e0d48d6f102ec04465aecd71d4b67b
Andrew Chittick <apChittick@gmail.com>
2019-10-31 19:10:54 -0400

    sends 2 strings to activity screen

--- app/src/main/java/com/example/rockpapers/DisplayResults.java
+++ app/src/main/java/com/example/rockpapers/DisplayResults.java
@@ -2,7 +2,9 @@ package com.example.rockpapers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayResults extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class DisplayResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_results);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String userMove = extras.getString("userMove");
+        String pcMove = extras.getString("pcMove");
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textViewLeft = findViewById(R.id.textView3);
+        textViewLeft.setText(userMove);
+
+        TextView textViewRight = findViewById(R.id.textView4);
+        textViewRight.setText(pcMove);
     }
 }
--- app/src/main/java/com/example/rockpapers/MainActivity.java
+++ app/src/main/java/com/example/rockpapers/MainActivity.java
@@ -11,17 +11,39 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    //public static final String EXTRA_MESSAGE = "bob";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void scissors_button(View view){
+    public void beginGame(View view){
+      //begin game
+      Intent intent = new Intent(this, DisplayResults.class);
+      String userMove = "hi";
+      String pcMove = "bob";
+      //Game game = new Game(button);
+      //String message = game.result;
+
+        //intent.putExtra(EXTRA_MESSAGE, userMove);
+
+      Bundle extras = new Bundle();
+      extras.putString("userMove", userMove);
+      extras.putString("pcMove", pcMove);
+      intent.putExtras(extras);
+
+      startActivity(intent);
+    }
+
+    public void ame(){
         //do stuff
-        Intent intent = new Intent(this, DisplayResults.class);
+        //Intent intent = new Intent(this, DisplayResults.class);
         //EditText editText = (EditText) findViewById(R.id.editText);
         //String message = editText.getText().toString();
+        //String message = "\ud83d\udc3b";
+        //result = message;
         //intent.putExtra(EXTRA_MESSAGE, message);
         //startActivity(intent);
     }
