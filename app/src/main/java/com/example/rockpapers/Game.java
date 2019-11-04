ecb38ec97569920455f185d534c0150e4e72a646
Andrew Chittick <apChittick@gmail.com>
2019-11-03 20:28:30 -0500

    game done

--- app/src/main/java/com/example/rockpapers/Game.java
+++ app/src/main/java/com/example/rockpapers/Game.java
@@ -58,4 +58,43 @@ class Game {
         return this.results;
     }
 
+    /*contains properties for moves*/
+    static class Move {
+        //private properties
+        private String gesture;
+        private String emoji;
+        private float rotation;
+        private String beats;
+
+        Move(String move){
+            gesture = move;
+            if (gesture.equals("rock")){
+                emoji = "\uD83D\uDC4A";
+                rotation = 270;
+                beats = "scissors";
+            }
+            else if (gesture.equals("paper")){
+                emoji = "✋️";
+                rotation = 0;
+                beats = "rock";
+            }
+            else {//scissors
+                emoji = "✌";
+                rotation = 270;
+                beats = "paper";
+            }
+        }
+        String getGesture(){
+            return this.gesture;
+        }
+        String getEmoji(){
+            return this.emoji;
+        }
+        float getRotation(){
+            return this.rotation;
+        }
+        String getBeats(){
+            return this.beats;
+        }
+    }
 }
